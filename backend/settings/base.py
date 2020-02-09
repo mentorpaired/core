57e21b44185c3c8a4af854566ee3d41a4a69eada
ailevi <nkoliokafor@gmail.com>
2020-02-09 21:24:27 +0100

    Add new lines to .env files and secret_key file Edit spelling error in base.py

--- backend/settings/base.py
+++ backend/settings/base.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'dango.middleware.common.CommonMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
