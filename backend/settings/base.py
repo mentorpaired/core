d3778ee82a6fcb525a5034d53e601ff50df14f75
ailevi <nkoliokafor@gmail.com>
2020-02-16 00:12:50 +0100

    Add initialised app to base.py

--- backend/settings/base.py
+++ backend/settings/base.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app',
 ]
 
 MIDDLEWARE = [
@@ -93,7 +94,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Berlin'
 
 USE_I18N = True
 
