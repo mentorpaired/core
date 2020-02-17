3a38401a10e3925f7767cf5abd5f3aa8ee4397b6
ailevi <nkoliokafor@gmail.com>
2020-02-17 22:23:10 +0100

    Add model objects to admin.py
    This is so that the objects show up on the admin interface

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import User, Stack, Spoken_Languages, Request, Interested_Mentor
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Stack)
+admin.site.register(Spoken_Languages)
+admin.site.register(Request)
+admin.site.register(Interested_Mentor)
