72beba8d53bf918139c776c7b6f2ad5ae77a40e4
ailevi <nkoliokafor@gmail.com>
2020-03-03 16:50:01 +0100

    Edit class names

--- app/admin.py
+++ app/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import User, Stack, Spoken_Languages, Request, Interested_Mentor
+from .models import User, Stack, SpokenLanguages, Request, InterestedMentor
 
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Stack)
-admin.site.register(Spoken_Languages)
+admin.site.register(SpokenLanguages)
 admin.site.register(Request)
-admin.site.register(Interested_Mentor)
+admin.site.register(InterestedMentor)
