10fb58eff392c12d80e98233c757df35fc75f09d
ailevi <nkoliokafor@gmail.com>
2020-03-03 23:28:08 +0100

    Create a readonly_field list in a class
    - Since user_id is set to editable=False in the models file, a class with a readonly_field for the id has to be created so that id can be displayed on the admin page

--- app/admin.py
+++ app/admin.py
@@ -2,7 +2,10 @@ from django.contrib import admin
 from .models import User, Stack, SpokenLanguages, Request, InterestedMentor
 
 # Register your models here.
-admin.site.register(User)
+class UserAdmin(admin.ModelAdmin):
+    readonly_fields=('user_id',)
+
+admin.site.register(User, UserAdmin)
 admin.site.register(Stack)
 admin.site.register(SpokenLanguages)
 admin.site.register(Request)
