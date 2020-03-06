34d889dcebfa5fca92f83f51b57506b0c2bb7728
ailevi <nkoliokafor@gmail.com>
2020-03-06 14:47:38 +0100

    Implement fixes for flake8
    - Remove trailing whitespaces
    - Break up text into chunks smaller than 100 characters on single lines
    - Add additional lines between class definitions

--- app/models.py
+++ app/models.py
@@ -9,10 +9,13 @@ class User(models.Model):
     mentor = models.BooleanField(default=False)
     mentee = models.BooleanField(default=True)
     user_id = models.UUIDField(
-        primary_key=True, 
-        default=uuid.uuid4, 
-        editable=False)
-    display_name = models.CharField(blank=False, max_length=300, help_text='Include first name and last name here')
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
+        )
+    display_name = models.CharField(
+        blank=False, max_length=300,
+        help_text='Include first name and last name here')
     about = models.TextField(max_length=1000, help_text='a brief description of you')
     avatar = CloudinaryField('image')
     stacks = models.ManyToManyField('Stack')
@@ -35,22 +38,25 @@ class User(models.Model):
     ALL_TIMEZONES = sorted((item, item) for item in pytz.all_timezones)
 
     timezone = models.CharField(max_length=300, choices=ALL_TIMEZONES)
-    availability = models.BooleanField(default=True, help_text='switch to false if you\'re not open to being matched')
-
+    availability = models.BooleanField(
+        default=True,
+        help_text='switch to false if you\'re not open to being matched'
+        )
 
     def __str__(self):
         return self.display_name
 
+
 class Stack(models.Model):
     name = models.CharField(max_length=100, null=False)
-    
+
     STACK_PROFICIENCY = [
         ('B', 'Beginner'),
         ('I', 'Intermediate'),
         ('A', 'Advanced')
     ]
     proficiency = models.CharField(
-        max_length=1, 
+        max_length=1,
         choices=STACK_PROFICIENCY,
         null=False
     )
@@ -58,9 +64,10 @@ class Stack(models.Model):
     def __str__(self):
         return self.name
 
+
 class SpokenLanguages(models.Model):
     name = models.CharField(max_length=100, null=False)
-    
+
     LANGUAGE_PROFICIENCY = [
         ('NP', 'No Proficiency'),
         ('EP', 'Elementary Proficiency'),
@@ -71,26 +78,41 @@ class SpokenLanguages(models.Model):
     ]
     proficiency = models.CharField(
         max_length=3,
-        choices = LANGUAGE_PROFICIENCY,
+        choices=LANGUAGE_PROFICIENCY,
         null=False
     )
 
     def __str__(self):
         return self.name
 
+
 class Request(models.Model):
     stack = models.ManyToManyField(Stack)
-    description = models.TextField(max_length=500, help_text='brief overview of what you\'d like to learn, your available days, etc')
+    description = models.TextField(
+        max_length=500,
+        help_text='brief overview of what you\'d like to learn, your available days, etc'
+        )
     mentee = models.ForeignKey(User, on_delete=models.CASCADE)
     interested_mentors = models.ManyToManyField('InterestedMentor')
-    matched_mentor = models.ForeignKey(User, related_name='Matched', on_delete=models.SET_NULL, null=True)
+    matched_mentor = models.ForeignKey(
+        User,
+        related_name='Matched',
+        on_delete=models.SET_NULL,
+        null=True
+        )
 
     def __str__(self):
         return self.mentee.display_name
 
+
 class InterestedMentor(models.Model):
     name = models.ForeignKey(User, on_delete=models.CASCADE)
-    personalised_note = models.TextField(max_length=1000, blank=False, help_text='say hi to your new mentee, include important details that you think will be helpful during the mentorship')
+    personalised_note = models.TextField(
+        max_length=1000,
+        blank=False,
+        help_text='''say hi to your new mentee, include important details that you think
+        will be helpful during the mentorship'''
+        )
     accepted = models.BooleanField(default=False)
 
     def __str__(self):
