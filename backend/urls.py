ef0aef4f5bce5cfabac6be2e4a34f3de84410366
ailevi <nkoliokafor@gmail.com>
2020-03-17 20:28:59 +0100

    Add GraphQL URL

--- backend/urls.py
+++ backend/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from graphene_django.views import GraphQLView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('app/', include('app.urls')),
+    path('graphql/', GraphQLView.as_view(graphiql=True)),
 ]
