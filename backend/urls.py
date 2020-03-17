c67b877406dd8522da4a36573139846da4a97efa
ailevi <nkoliokafor@gmail.com>
2020-03-17 22:16:49 +0100

    Add import and code to disable Django CSRF protection

--- backend/urls.py
+++ backend/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from graphene_django.views import GraphQLView
+from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('app/', include('app.urls')),
-    path('graphql/', GraphQLView.as_view(graphiql=True)),
+    path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
 ]
