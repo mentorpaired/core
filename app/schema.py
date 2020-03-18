1e503c40ac6e4221475c2a0e3e40fc429a381c2c
ailevi <nkoliokafor@gmail.com>
2020-03-18 22:48:31 +0100

    Recreate Query types using Relay

--- app/schema.py
+++ app/schema.py
@@ -1,47 +1,40 @@
 import graphene
+import django_filters
 from graphene_django import DjangoObjectType
+from graphene_django.filter import DjangoFilterConnectionField
 
 from .models import User, Stack, SpokenLanguage, Request, InterestedMentor
 
-# The types below are objects (models) that contain fields 
-class UserType(DjangoObjectType):
-    class Meta:
-        model = User
+# The types below are objects (models) that contain fields, created using relay
+class UserNode(DjangoObjectType):
+    model = User
+    fields = ['mentor', 'mentee', 'user_id', 'display_name', 'about', 'avatar' 'stacks', 'preferred_pronouns', 'spoken_languages', 'website', 'timezone', 'availability']
+    interfaces = (graphene.relay.Node, )
 
-class StackType(DjangoObjectType):
-    class Meta:
-        model = Stack
+class StackNode(DjangoObjectType):
+    model = Stack
+    fields = ['name', 'proficiency']
+    interfaces = (graphene.relay.Node, )
 
-class SpokenLanguageType(DjangoObjectType):
-    class Meta:
-        model = SpokenLanguage
+class SpokenLanguageNode(DjangoObjectType):
+    model = SpokenLanguage
+    fields = ['name', 'proficiency']
+    interfaces = (graphene.relay.Node, )
 
-class RequestType(DjangoObjectType):
-    class Meta:
-        model = Request
+class RequestNode(DjangoObjectType):
+    model = Request
+    fields = ['stack', 'description', 'mentee', 'interested_mentors', 'matched_mentor']
+    interfaces = (graphene.relay.Node, )
 
-class InterestedMentorType(DjangoObjectType):
-    class Meta:
-        model = InterestedMentor
+class InterestedMentorNode(DjangoObjectType):
+    model = InterestedMentor
+    fields = ['name', 'personalised_note', 'accepted']
+    interfaces = (graphene.relay.Node, )
 
+# Create Query
 class Query(graphene.ObjectType):
-    users = graphene.List(UserType)
-    stacks = graphene.List(StackType)
-    spoken_languages = graphene.List(SpokenLanguageType)
-    requests = graphene.List(RequestType)
-    interested_mentors = graphene.List(InterestedMentorType)
-
-    def resolve_users(self, info, **kwargs):
-        return User.objects.all()
-
-    def resolve_stacks(self, info, **kwargs):
-        return Stack.objects.all()
-
-    def resolve_spoken_languages(self, info, **kwargs):
-        return SpokenLanguage.objects.all()
-
-    def resolve_requests(self, info, **kwargs):
-        return Request.objects.all()
-
-    def resolve_interested_mentors(self, info, **kwargs):
-        return InterestedMentor.objects.all()
+    relay_user = graphene.relay.Node.Field(UserNode)
+    relay_stack = graphene.relay.Node.Field(StackNode)
+    relay_spoken_language = graphene.relay.Node.Field(SpokenLanguageNode)
+    relay_request = graphene.relay.Node.Field(RequestNode)
+    relay_interested_mentor = graphene.relay.Node.Field(InterestedMentorNode) 
