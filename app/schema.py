952f442e976b04791b74568086759f5a2d90a92f
ailevi <nkoliokafor@gmail.com>
2020-03-19 14:15:43 +0100

    Use Relay to create DjangoObjectTypes for the models

--- app/schema.py
+++ app/schema.py
@@ -1,30 +1,49 @@
 import graphene
-import django_filters
+# import django_filters
 from graphene_django import DjangoObjectType
-from graphene_django.filter import DjangoFilterConnectionField
+# from graphene_django.filter import DjangoFilterConnectionField
+
 
 from .models import User, Stack, SpokenLanguage, Request, InterestedMentor
 
 # The types below are objects (models) that contain fields, created using relay
+
+
 class UserNode(DjangoObjectType):
     model = User
-    fields = ['mentor', 'mentee', 'user_id', 'display_name', 'about', 'avatar' 'stacks', 'preferred_pronouns', 'spoken_languages', 'website', 'timezone', 'availability']
+    fields = [
+        'mentor',
+        'mentee',
+        'user_id',
+        'display_name',
+        'about',
+        'avatar',
+        'stacks',
+        'preferred_pronouns',
+        'spoken_languages',
+        'website',
+        'timezone',
+        'availability']
     interfaces = (graphene.relay.Node, )
+
 
 class StackNode(DjangoObjectType):
     model = Stack
     fields = ['name', 'proficiency']
     interfaces = (graphene.relay.Node, )
 
+
 class SpokenLanguageNode(DjangoObjectType):
     model = SpokenLanguage
     fields = ['name', 'proficiency']
     interfaces = (graphene.relay.Node, )
 
+
 class RequestNode(DjangoObjectType):
     model = Request
     fields = ['stack', 'description', 'mentee', 'interested_mentors', 'matched_mentor']
     interfaces = (graphene.relay.Node, )
+
 
 class InterestedMentorNode(DjangoObjectType):
     model = InterestedMentor
@@ -32,9 +51,11 @@ class InterestedMentorNode(DjangoObjectType):
     interfaces = (graphene.relay.Node, )
 
 # Create Query
+
+
 class Query(graphene.ObjectType):
     relay_user = graphene.relay.Node.Field(UserNode)
     relay_stack = graphene.relay.Node.Field(StackNode)
     relay_spoken_language = graphene.relay.Node.Field(SpokenLanguageNode)
     relay_request = graphene.relay.Node.Field(RequestNode)
-    relay_interested_mentor = graphene.relay.Node.Field(InterestedMentorNode) 
+    relay_interested_mentor = graphene.relay.Node.Field(InterestedMentorNode)
