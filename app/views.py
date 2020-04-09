3828d89b314ac10f2c97d2256b5946792aec59a2
ailevi <nkoliokafor@gmail.com>
2020-04-09 21:20:27 +0100

    Add viewsets for the serializers

--- app/views.py
+++ app/views.py
@@ -1,3 +1,43 @@
-# from django.shortcuts import render
+from app.models import Stack, SpokenLanguage, User, InterestedMentor, Request
+from app.serializers import StackSerializer, SpokenLanguageSerializer, UserSerializer, InterestedMentorSerializer, RequestSerializer
+from rest_framework import viewsets
 
-# Create your views here.
+
+class StackViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing stacks.
+    """
+    queryset = Stack.objects.all()
+    serializer_class = StackSerializer
+
+
+class SpokenLanguageViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing the language(s) a user speaks.
+    """
+    queryset = SpokenLanguage.objects.all()
+    serializer_class = SpokenLanguageSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing the users.
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class InterestedMentorViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing the interested mentors.
+    """
+    queryset = InterestedMentor.objects.all()
+    serializer_class = InterestedMentorSerializer
+
+
+class RequestViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing mentee's requests.
+    """
+    queryset = Request.objects.all()
+    serializer_class = RequestSerializer
