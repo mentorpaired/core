421b4a35616ae760d0c47971946405e99b49d573
ailevi <nkoliokafor@gmail.com>
2020-04-10 17:08:19 +0100

    Make changes to serializer relation type
    - Switched out hyperlinkedmodelserializer for modelserializer
    - Added primarykeyfield relations to the parent models

--- app/serializers.py
+++ app/serializers.py
@@ -2,19 +2,22 @@ from app.models import Stack, SpokenLanguage, User, InterestedMentor, Request
 from rest_framework import serializers
 
 
-class StackSerializer(serializers.HyperlinkedModelSerializer):
+class StackSerializer(serializers.PrimaryKeyRelatedField, serializers.ModelSerializer):
     class Meta:
         model = Stack
         fields = ['name', 'proficiency']
 
 
-class SpokenLanguageSerializer(serializers.HyperlinkedModelSerializer):
+class SpokenLanguageSerializer(serializers.PrimaryKeyRelatedField, serializers.ModelSerializer):
     class Meta:
         model = SpokenLanguage
         fields = ['name', 'proficiency']
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
+    stacks = StackSerializer(many=True, queryset=Stack.objects.all())
+    spoken_languages = SpokenLanguageSerializer(many=True, queryset=SpokenLanguage.objects.all())
+
     class Meta:
         model = User
         fields = [
@@ -32,7 +35,7 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
         ]
 
 
-class InterestedMentorSerializer(serializers.HyperlinkedModelSerializer):
+class InterestedMentorSerializer(serializers.ModelSerializer):
     class Meta:
         model = InterestedMentor
         fields = [
@@ -42,7 +45,7 @@ class InterestedMentorSerializer(serializers.HyperlinkedModelSerializer):
         ]
 
 
-class RequestSerializer(serializers.HyperlinkedModelSerializer):
+class RequestSerializer(serializers.ModelSerializer):
     class Meta:
         model = Request
         fields = [
