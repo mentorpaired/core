2af0fd9569c00a33c24f5886866b5beb9966e25b
ailevi <nkoliokafor@gmail.com>
2020-04-14 22:46:19 +0100

    Add stacks and languages to be able to view changes in their objects

--- app/urls.py
+++ app/urls.py
@@ -5,6 +5,8 @@ from rest_framework.routers import DefaultRouter
 # Using a router to register the viewsets
 router = DefaultRouter()
 router.register(r'users', views.UserViewSet)
+router.register(r'stacks', views.StackViewSet)
+router.register(r'languages', views.SpokenLanguageViewSet)
 
 # API URLs are determined automatically by the router
 urlpatterns = [
