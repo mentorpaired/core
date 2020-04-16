a264f411caafccdee809053f8cb9995a50e4e9c6
ailevi <nkoliokafor@gmail.com>
2020-04-16 17:26:04 +0100

    Add new line

--- app/views.py
+++ app/views.py
@@ -4,8 +4,9 @@ from rest_framework.viewsets import GenericViewSet
 from .models import Stack, SpokenLanguage, User
 from .serializers import StackSerializer, SpokenLanguageSerializer, UserSerializer
 
+
 class StackViewSet(GenericViewSet, CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, ListModelMixin, DestroyModelMixin):
-    
+
     serializer_class = StackSerializer
     queryset = Stack.objects.all()
 
