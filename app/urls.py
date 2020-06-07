152dd4e02f2e3789e5fe6d525f126e8c5805f360
Tbones41 <tbones4665@gmail.com>
2020-06-07 19:30:43 +0100

    Secure api endpoint to solve issue #22

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from rest_framework_simplejwt import views as jwt_views
-from rest_framework.authtoken.views import obtain_auth_token
 
 from .views.language import (LanguageDetail, LanguageList,
                              LanguageProficiencyDetail,
