b2c9044d167488c11c6c8fcd9ed424e6b9f1716c
Tbones41 <tbones4665@gmail.com>
2020-06-20 13:44:22 +0100

    rebase and clear index

--- app/urls.py
+++ app/urls.py
@@ -11,10 +11,10 @@ from .views.user import UserDetail, UserList
 urlpatterns = [
     path(
         'api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'
-    ),
+        ),
     path(
         'api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'
-    ),
+        ),
     path(
         'skillproficiency/',
         SkillProficiencyList.as_view(),
