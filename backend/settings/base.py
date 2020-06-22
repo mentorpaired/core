cc09d83b7af634bf796066acc8119c29985dbc3f
Tbones41 <tbones4665@gmail.com>
2020-06-22 02:36:30 +0100

    Comment out hanging curly bracket

--- backend/settings/base.py
+++ backend/settings/base.py
@@ -169,4 +169,4 @@ REST_FRAMEWORK = {
     # 'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
     # 'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
     # 'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
-}
+# }
