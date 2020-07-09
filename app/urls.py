07ebb0bd037a2afad368b118b12b61cc60188dae
ailevi <nkoliokafor@gmail.com>
2020-07-09 20:29:12 +0100

    Create new url mapping for github authentication

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,6 @@
-from django.urls import path
-
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
-
+from .views.user import UserDetail, UserList
+from .views.skill import (SkillDetail, SkillList, SkillProficiencyDetail,
+                          SkillProficiencyList)
 from .views.language import (LanguageDetail, LanguageList,
                              LanguageProficiencyDetail,
                              LanguageProficiencyList)
@@ -12,69 +8,78 @@ from .views.skill import (SkillDetail, SkillList, SkillProficiencyDetail,
                           SkillProficiencyList)
 from .views.user import UserDetail, UserList
 from .views.request import RequestDetail, RequestList
+from django.urls import path
+from rest_framework_simplejwt.views import (TokenObtainPairView,
+                                            TokenRefreshView)
+
+from .views.github_auth import GithubAuthenticationView
+
 
 urlpatterns = [
     path(
         'api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'
-        ),
+    ),
     path(
         'api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'
-        ),
+    ),
+    path(
+        'github-oauth/$', GithubAuthenticationView.as_view(), name='github_authenticate'
+    ),
     path(
         'skillproficiency/',
         SkillProficiencyList.as_view(),
         name='proficiency_list'
-        ),
+    ),
     path(
         'skillproficiency/<int:pk>/',
         SkillProficiencyDetail.as_view(),
         name='proficiency_detail'
-        ),
+    ),
     path(
         'skills/',
         SkillList.as_view(),
         name='skill_list'
-        ),
+    ),
     path(
         'skills/<int:pk>/',
         SkillDetail.as_view(),
         name='skill_list'
-        ),
+    ),
     path(
         'languageproficiency/',
         LanguageProficiencyList.as_view(),
         name='languageproficiency_list'
-        ),
+    ),
     path(
         'languageproficiency/<int:pk>/',
         LanguageProficiencyDetail.as_view(),
         name='languageproficiency_detail'
-        ),
+    ),
     path(
         'languages/',
         LanguageList.as_view(),
         name='language_list'
-        ),
+    ),
     path(
         'languages/<int:pk>/',
         LanguageDetail.as_view(),
         name='language_detail'
-        ),
+    ),
     path(
         'users/',
         UserList.as_view(),
         name='user_list'
-        ),
+    ),
     path(
         'users/<uuid:pk>/',
         UserDetail.as_view(),
         name='user_detail'
-        ),
+    ),
     path(
         'requests/',
         RequestList.as_view(),
         name='request_list'
-        ),
+    ),
     path(
         'requests/<int:pk>/',
         RequestDetail.as_view(),
