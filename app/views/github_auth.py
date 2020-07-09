d68995121fc6938d325a21b9d47e4f4168085710
ailevi <nkoliokafor@gmail.com>
2020-07-09 20:34:05 +0100

    Create new view file for githubauth serializer

---
+++ app/views/github_auth.py
@@ -0,0 +1,47 @@
+import os
+
+from dotenv import load_dotenv
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from ..oauth import (convert_to_auth_token, generate_github_access_token,
+                     get_user_from_token)
+from ..serializers import GithubAuthSerializer
+
+
+load_dotenv()
+
+# Github client id and client secret
+SOCIAL_AUTH_GITHUB_KEY = os.getenv('SOCIAL_AUTH_GITHUB_KEY')
+SOCIAL_AUTH_GITHUB_SECRET = os.getenv('SOCIAL_AUTH_GITHUB_SECRET')
+
+# Oauth Toolkit id and secret
+CLIENT_ID = os.getenv('DJANGO_OAUTH_APP_CLIENT_ID')
+CLIENT_SECRET = os.getenv('DJANGO_OAUTH_APP_CLIENT_SECRET')
+
+
+class GithubAuthenticationView(APIView):
+
+    def post(self, request):
+        github_token = generate_github_access_token(
+            github_client_id=SOCIAL_AUTH_GITHUB_KEY,
+            github_client_secret=SOCIAL_AUTH_GITHUB_SECRET,
+            github_code=request.data['code']
+        )
+
+        django_auth_token = convert_to_auth_token(
+            client_id=CLIENT_ID,
+            client_secret=CLIENT_SECRET,
+            backend='github',
+            token=github_token
+        )
+        github_user = get_user_from_token(django_auth_token)
+
+        return Response(
+            {
+                'token': django_auth_token,
+                'user': GithubAuthSerializer(github_user).data
+            },
+            status=status.HTTP_201_CREATED
+        )
