d629c93879990994c11948e5caf02e22eb12cc2f
ailevi <nkoliokafor@gmail.com>
2020-10-24 13:20:53 +0100

    Update tests with non-suplicate email fields

--- app/tests/base.py
+++ app/tests/base.py
@@ -1,66 +1,63 @@
 from rest_framework.test import APIClient, APITestCase
 
-from ..models import (LanguageProficiency, Pronoun, Request, Role, Skill,
-                      SkillProficiency, SpokenLanguage, User)
+from ..models import (
+    LanguageProficiency,
+    Pronoun,
+    Request,
+    Role,
+    Skill,
+    SkillProficiency,
+    SpokenLanguage,
+    User,
+)
 
 
 class BaseTestCase(APITestCase):
-
     def setUp(self):
 
         self.client = APIClient()
 
         self.admin = User.objects.create_superuser(
-            username='test',
-            email='test@test.com',
-            password='testpassword'
+            username="test", email="test@test.com", password="testpassword"
         )
 
-        self.assertEqual(self.admin.is_active, 1, 'Active User')
+        self.assertEqual(self.admin.is_active, 1, "Active User")
 
-        self.admin_login = self.client.post('/api/token/', {
-            'username': 'test',
-            'password': 'testpassword',
-        }, format='json')
+        self.admin_login = self.client.post(
+            "/api/token/",
+            {
+                "username": "test",
+                "password": "testpassword",
+            },
+            format="json",
+        )
 
         self.assertEqual(self.admin_login.status_code, 200)
 
-        self.token = self.admin_login.data['access']
+        self.token = self.admin_login.data["access"]
 
-        self.role = Role.objects.create(
-            role="Test"
-        )
-        self.role2 = Role.objects.create(
-            role="Test2"
-        )
-        self.pronoun = Pronoun.objects.create(
-            pronoun="Test"
-        )
-        self.skillproficiency = SkillProficiency.objects.create(
-            level="X"
-        )
+        self.role = Role.objects.create(role="Test")
+        self.role2 = Role.objects.create(role="Test2")
+        self.pronoun = Pronoun.objects.create(pronoun="Test")
+        self.skillproficiency = SkillProficiency.objects.create(level="X")
         self.skill = Skill.objects.create(
-            name="Test Programming Language",
-            proficiency=self.skillproficiency
+            name="Test Programming Language", proficiency=self.skillproficiency
         )
         self.skill2 = Skill.objects.create(
-            name="Another test programming language",
-            proficiency=self.skillproficiency
+            name="Another test programming language", proficiency=self.skillproficiency
         )
-        self.languageproficiency = LanguageProficiency.objects.create(
-            level="X"
-        )
+        self.languageproficiency = LanguageProficiency.objects.create(level="X")
         self.spoken_language = SpokenLanguage.objects.create(
-            name="Test Spoken Language",
-            proficiency=self.languageproficiency
+            name="Test Spoken Language", proficiency=self.languageproficiency
         )
         self.profile = User.objects.create(
             username="testuser",
+            email="testemail@testemail.com",
             about="Lorem Ipsum text",
             avatar="https://dummyavatars.githubcontent.com/u/1",
             pronoun=self.pronoun,
             website="www.test.com",
-            timezone="Testcontinent/testcountry"
+            timezone="Testcontinent/testcountry",
         )
         self.profile.role.add(self.role.id)
         self.profile.skills.add(self.skill.id)
@@ -69,11 +66,12 @@ class BaseTestCase(APITestCase):
 
         self.request_mentee = User.objects.create(
             username="Second user",
+            email="requestmentee@email.com",
             about="Random bio of second user",
             avatar="https://dummyavatars.githubcontent.com/u/0",
             pronoun=self.pronoun,
             website="www.secondtestsite.com",
-            timezone="Secondcontinent/secondcountry"
+            timezone="Secondcontinent/secondcountry",
         )
         self.request_mentee.role.add(self.role2.id)
         self.request_mentee.skills.add(self.skill2.id)
@@ -84,7 +82,7 @@ class BaseTestCase(APITestCase):
             skill=self.skill2,
             description="Short text about a request for mentorship",
             mentee=self.request_mentee,
-            status="OPEN"
+            status="OPEN",
         )
 
     def tearDown(self):
--- app/tests/test_github_oauth.py
+++ app/tests/test_github_oauth.py
@@ -11,45 +11,49 @@ class TestGithubOauth(unittest.TestCase):
     def setUp(self):
         self.client = APIClient()
         self.user = User.objects.create_user(
-            user_id=1,
-            username='test'
+            user_id=1, username="test", email="test@test.com"
         )
 
-    @patch('app.views.github_oauth.get_user_from_token')
-    @patch('app.views.github_oauth.convert_to_auth_token')
-    @patch('app.views.github_oauth.retrieve_github_user_info')
-    @patch('app.views.github_oauth.generate_github_access_token')
-    @patch('app.views.github_oauth.get_refresh_access_token')
-    def test_github_authenticate(self, get_refresh_access_token, generate_access_token,
-                                 retrieve_github_user, convert_to_auth_token,
-                                 get_user_from_token):
+    @patch("app.views.github_oauth.get_user_from_token")
+    @patch("app.views.github_oauth.convert_to_auth_token")
+    @patch("app.views.github_oauth.retrieve_github_user_info")
+    @patch("app.views.github_oauth.generate_github_access_token")
+    @patch("app.views.github_oauth.get_refresh_access_token")
+    def test_github_authenticate(
+        self,
+        get_refresh_access_token,
+        generate_access_token,
+        retrieve_github_user,
+        convert_to_auth_token,
+        get_user_from_token,
+    ):
 
         get_refresh_access_token.return_value = {
-            'refresh': 'randomrefreshtoken',
-            'access': 'randomaccesstoken'
+            "refresh": "randomrefreshtoken",
+            "access": "randomaccesstoken",
         }
 
-        generate_access_token.return_value = 'token'
+        generate_access_token.return_value = "token"
 
         github_user = {
-            'user_id': 'id1',
-            'avatar_url': 'https://dummyavatar.com/v0',
-            'email': 'test@test.com',
-            'location': 'CEST'
+            "user_id": "id1",
+            "avatar_url": "https://dummyavatar.com/v0",
+            "email": "test@test.com",
+            "location": "CEST",
         }
 
         retrieve_github_user.return_value = github_user
 
-        convert_to_auth_token.return_value = 'auth_token'
+        convert_to_auth_token.return_value = "auth_token"
         get_user_from_token.return_value = self.user
 
         mock_data = {
-            'client_id': 'somerandomstring',
-            'client_secret': 'anotherlongerrandomstring',
-            'code': 'arandomstring01'
+            "client_id": "somerandomstring",
+            "client_secret": "anotherlongerrandomstring",
+            "code": "arandomstring01",
         }
 
-        response = self.client.post('/github_auth/', data=mock_data)
+        response = self.client.post("/github_auth/", data=mock_data)
         assert response.status_code == 201
 
         expected_response = {
@@ -59,20 +63,21 @@ class TestGithubOauth(unittest.TestCase):
                 "user_id": "id1",
                 "avatar_url": "https://dummyavatar.com/v0",
                 "email": "test@test.com",
-                "location": "CEST"
+                "location": "CEST",
             },
-            'user': {
-                'user_id': '1',
-                'username': 'test',
-                'role': [],
-                'about': '',
-                'avatar': 'https://dummyavatar.com/v0',
-                'skills': [],
-                'pronoun': None,
-                'spoken_languages': [],
-                'timezone': 'CEST',
-                'availability': True
-            }
+            "user": {
+                "user_id": "1",
+                "username": "test",
+                "email": "test@test.com",
+                "role": [],
+                "about": "",
+                "avatar": "https://dummyavatar.com/v0",
+                "skills": [],
+                "pronoun": None,
+                "spoken_languages": [],
+                "timezone": "CEST",
+                "availability": True,
+            },
         }
         assert json.loads(response.content) == json.loads(json.dumps(expected_response))
 
