31dc56a331305d1139a0b62475a9850af9d8a184
ailevi <nkoliokafor@gmail.com>
2020-11-29 12:38:08 +0100

    Edit init and test files for github actions postgres service

--- backend/settings/test.py
+++ backend/settings/test.py
@@ -1,7 +1,7 @@
 from .base import *
 
 # Secret Key
-SECRET_KEY = 'Random secret key'
+SECRET_KEY = "Random secret key"
 
 # Debug
 DEBUG = True
@@ -13,12 +13,12 @@ ALLOWED_HOSTS = []
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'travis_ci_test',
-        'USER': 'postgres',
-        'PASSWORD': '',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "github_actions",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
