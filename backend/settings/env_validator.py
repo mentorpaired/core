68637828c56b20718903831548868375bff57260
ailevi <nkoliokafor@gmail.com>
2021-03-10 11:43:52 +0100

    create list variable for env variables and add validator function to production env

--- backend/settings/env_validator.py
+++ backend/settings/env_validator.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-def validate_env_vars(*args):
+def validate_env_vars(args):
     for arg in args:
         if not os.getenv(arg):
             sys.exit(f"{arg} environment variable is required.")
