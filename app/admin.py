f065606ab238ab2bd50330f1eb4d2e3e500c9496
ailevi <nkoliokafor@gmail.com>
2021-04-30 13:56:02 +0200

    create new goals model, create migration and add to admin page

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import User, Skill, SpokenLanguage, Request, RequestInterest
+from .models import User, Skill, SpokenLanguage, Request, RequestInterest, Goal
 
 
 admin.site.register(User)
@@ -8,3 +8,4 @@ admin.site.register(Skill)
 admin.site.register(SpokenLanguage)
 admin.site.register(Request)
 admin.site.register(RequestInterest)
+admin.site.register(Goal)
